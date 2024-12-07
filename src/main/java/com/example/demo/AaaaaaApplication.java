9995ed8789f43f7dba009bdc21668eac1ab8a919
wlzhd <jtr6120@naver.com>
2024-12-07 14:09:00 +0900

    init

---
+++ src/main/java/com/example/demo/AaaaaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaaaaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaaaaaApplication.class, args);
+	}
+
+}
